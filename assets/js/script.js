ff0d562aa1cfa69f8d5601bd737d5182cf4d4d5b
qasimushtaq <134164364+qasimushtaq@users.noreply.github.com>
2023-09-27 17:08:50 +0000

    js code for menu togglr for small screens

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//Code for Menu toggle
+var MenuItems = document.getElementById('MenuItems');
+MenuItems.style.maxHeight = '0px';
+function menutoggle() {
+    if (MenuItems.style.maxHeight == '0px') {
+        MenuItems.style.maxHeight = '200px';
+    } else {
+        MenuItems.style.maxHeight = '0px';
+    }
+}
